3f87149298384bbfed7168d76481a0b2aa012ba6
Naoaki Obiki <obiki.wk@gmail.com>
2016-11-20 20:27:38 +0900

    modified:   .python-version 	new file:   tsl_graph/luxes.png 	modified:   tsl_graph/test.py

--- tsl_graph/test.py
+++ tsl_graph/test.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python
 import pandas as pd
 import matplotlib as plt
-plt.use('TkAgg')
+plt.use('Agg')
 
-df = pd.read_csv('luxes.csv')
+df = pd.read_csv("tsl_graph/luxes.csv")
 # df.index = pd.to_datetime(df.index)
 df.plot(x='datetime')
 
 # plt.pyplot.show()
-plt.pyplot.savefig("luxes.png")
+plt.pyplot.savefig("tsl_graph/luxes.png")
