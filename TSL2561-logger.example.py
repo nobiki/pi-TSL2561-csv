d77180991a6bb7697998e91fcfff9ff22090749a
Naoaki Obiki <obiki.wk@gmail.com>
2016-11-22 01:52:02 +0900

    oursql test

--- TSL2561-logger.example.py
+++ TSL2561-logger.example.py
@@ -15,20 +15,37 @@ from TSL2561_logger import *
 tsl = TSL2561()
 logger = TSL2561_logger()
 
-if tsl.foundSensor():
-    print("Found sensor...")
+def sample_oursql():
+    import oursql
+    with oursql.connect(
+            host='my-gandamu',
+            db='tsl2561_stats_development',
+            user='9zilla',
+            passwd='9zilla').cursor() as cur:
 
-    tsl.setGain(tsl.GAIN_16X);
-    tsl.setTiming(tsl.INTEGRATIONTIME_13MS)
+        print(cur.execute('SELECT * FROM luxes'))
+        res = cur.fetchall()
 
-    x = tsl.getFullLuminosity()
-    full = tsl.getLuminosity(tsl.FULLSPECTRUM)
-    visible = tsl.getLuminosity(tsl.VISIBLE)
-    infrared = tsl.getLuminosity(tsl.INFRARED)
+        for row in res:
+            print(row)
 
-    logger.hello()
 
-    print("Lux: %d" % tsl.calculateLux(full, infrared) )
-else:
-    print("No sensor?")
+sample_oursql()
+
+# if tsl.foundSensor():
+#     print("Found sensor...")
+#
+#     tsl.setGain(tsl.GAIN_16X);
+#     tsl.setTiming(tsl.INTEGRATIONTIME_13MS)
+#
+#     x = tsl.getFullLuminosity()
+#     full = tsl.getLuminosity(tsl.FULLSPECTRUM)
+#     visible = tsl.getLuminosity(tsl.VISIBLE)
+#     infrared = tsl.getLuminosity(tsl.INFRARED)
+#
+#     logger.hello()
+#
+#     print("Lux: %d" % tsl.calculateLux(full, infrared) )
+# else:
+#     print("No sensor?")
 
