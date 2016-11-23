4fc6acfe35d6249e42c5d355e88d1d242c74305b
Naoaki Obiki <obiki.wk@gmail.com>
2016-11-23 16:06:46 +0900

    worker test

--- worker.py
+++ worker.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
+import sys
 import time
+import logging
 import multiprocessing
+from datetime import datetime, timedelta
+from crontab import CronTab
 
 def worker(num):
     """worker function"""
     print("Worker " + str(i) + " start.")
     time.sleep(i*2)
-    print("Worker " + str(i) + " end.")
+    print("Worker " + str(i) + " end. - " + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
     return
 
 if __name__ == '__main__':
