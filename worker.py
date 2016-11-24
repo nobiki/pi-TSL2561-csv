8abc09d15f9c8b40e5084f94d3d20c00f4545b99
John Smith <john@example.com>
2016-11-24 14:49:02 +0900

    path

--- worker.py
+++ worker.py
@@ -8,7 +8,7 @@ import base64
 
 import oursql
 import yaml
-f = open(os.path.dirname(__file__)+"database.yml", 'r')
+f = open(os.path.dirname(__file__)+"/database.yml", 'r')
 db = yaml.load(f)
 f.close()
 
@@ -66,11 +66,11 @@ class TSL2561Logger(object) :
         df = pd.read_sql(sql,conn)
 
         df.plot(x='recorded_at')
-        plt.pyplot.savefig(os.path.dirname(__file__)+"luxes.png")
+        plt.pyplot.savefig(os.path.dirname(__file__)+"/luxes.png")
 
         # insert binary(base64)
         now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-        graphfile = open(os.path.dirname(__file__)+"luxes.png", 'rb').read()
+        graphfile = open(os.path.dirname(__file__)+"/luxes.png", 'rb').read()
         graph64 = base64.b64encode(graphfile).decode('utf-8')
 
         sql = "update " + db["name"] +".graphs" \
